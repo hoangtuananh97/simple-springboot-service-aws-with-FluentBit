1548752a8e3d677a2261e8ae3c8ee754a3c21ad1
tunanhh <tunanhh64@gmail.com>
2025-03-15 17:05:27 +0700

    fix code

--- springboot-application-aws-infra/lib/springboot-fargate-cdk-stack.ts
+++ springboot-application-aws-infra/lib/springboot-fargate-cdk-stack.ts
@@ -11,8 +11,8 @@ export class SpringbootFargateCdkStack  extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const s3Bucket = new s3.Bucket(this, 'hta-example-testing1234', {
-      bucketName: 'hta-example-testing1234',
+    const s3Bucket = new s3.Bucket(this, 'hta-example-testing12345', {
+      bucketName: 'hta-example-testing12345',
       publicReadAccess: false,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,

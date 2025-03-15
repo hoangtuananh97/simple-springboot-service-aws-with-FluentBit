aec7fc8cc6e182713753bab6d8ed332ab087d2f8
tunanhh <tunanhh64@gmail.com>
2025-03-15 09:49:42 +0700

    fix code

--- springboot-application-aws-infra/bin/springboot-fargate-cdk.ts
+++ springboot-application-aws-infra/bin/springboot-fargate-cdk.ts
@@ -6,7 +6,7 @@ import {SpringbootFargateCdkStack} from '../lib/springboot-fargate-cdk-stack';
 const app = new cdk.App();
 const DEFAULT_REGION = 'ap-northeast-1';
 
-new SpringbootFargateCdkStack(app, 'HTAExampleApp', {
+new SpringbootFargateCdkStack(app, 'SpringbootFargateCdkStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: DEFAULT_REGION,

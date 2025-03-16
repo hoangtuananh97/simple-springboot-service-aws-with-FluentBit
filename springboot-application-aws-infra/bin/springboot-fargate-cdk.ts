f18f35dd2b4da875b53dafde2389c42b8ea6fcbe
tunanhh <tunanhh64@gmail.com>
2025-03-16 09:05:21 +0700

    fix code

--- springboot-application-aws-infra/bin/springboot-fargate-cdk.ts
+++ springboot-application-aws-infra/bin/springboot-fargate-cdk.ts
@@ -5,7 +5,9 @@ import {SpringbootFargateCdkStack} from '../lib/springboot-fargate-cdk-stack';
 
 const app = new cdk.App();
 const DEFAULT_REGION = 'ap-southeast-1';
+const branchName = app.node.tryGetContext('BranchName') || 'main';
 
+console.log("branchName: ", branchName);
 new SpringbootFargateCdkStack(app, 'SpringbootFargateCdkStackTesting2', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
